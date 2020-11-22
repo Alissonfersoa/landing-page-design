7ed81e6bdf64a8b83d9573afed904b816c38e125
AlissonPC <batista.als@gmail.com>
2020-11-22 12:23:19 -0300

    Update Itens
    Add @media in CSS document

--- java.js
+++ java.js
@@ -10,3 +10,13 @@ function changeBgColor(color){
     const sec = document.querySelector('.sec');
     sec.style.background = color;
 }
+
+//função para layout responsivo
+
+function menuToggle(){
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navigation = document.querySelector('.navigation');
+    toggleMenu.classList.toggle('active');
+    navigation.classList.toggle('active');
+
+}
